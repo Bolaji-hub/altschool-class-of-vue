46d1d2aabc4fb51cf23863638d8c5622dd9ed252
Barakat Bolaji <barakatbolaji65@gmail.com>
2023-02-07 13:38:22 +0100

    Etract Game Picker into Coposable

---
+++ src/composables/useGamePicker.js
@@ -0,0 +1,15 @@
+export default function useGamePicker(colors, message) {
+  const matchColor = (value) => {
+    // do a random color based on the array index;
+    const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
+
+    if (colors[randomNumber] === value) {
+      message.value = `You win... [answer: ${colors[randomNumber]}]`;
+      return;
+    }
+
+    message.value = `You loose [answer: ${colors[randomNumber]}]`;
+  };
+
+  return matchColor
+}
